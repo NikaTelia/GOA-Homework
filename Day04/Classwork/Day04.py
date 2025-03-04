2ce821dcb287c29d0e43ca1dc2e2938d91144a7b
NikaTelia <nn4441135play@gmail.com>
2025-03-04 18:05:44 +0400

    commit

---
+++ Day04/Classwork/Day04.py
@@ -0,0 +1,16 @@
+# ----Classwork N1----
+
+Name = "nika"
+Lastname = "Telia"
+Age = 12
+
+Name = Name + "!"
+Lastname = Lastname + "!"
+Age = Age + 1
+print (Age,Lastname,Name)
+
+# ----Classwork N2----
+
+name = "Gurami"
+age = "15"
+print(name + age)
---
+++ Day04/Homework/Day04.py
@@ -0,0 +1,24 @@
+#---დავალება N1---
+
+x = 1
+y = 2
+z = 3
+print (x,y,z)
+
+#---დავალება N2---
+
+Num1 = 2
+Num2 = 3
+print (Num1 + Num2)
+
+#---დავალება N3---
+
+Age = 12
+print (Age)
+Age = 13
+print (Age)
+
+#---დავალება N4---
+
+number = 5
+print("Result:" + str(number))
