065f684fb65ff23a415d220dd592ee2afbbcea0f
Nika Telia <nn4441135play@gmail.com>
2026-05-03 21:17:28 +0400

    commit

---
+++ Day46/Homework/script.js
@@ -0,0 +1,21 @@
+function changeColor() {
+  document.getElementById("text").style.color = "red";
+}
+
+function changeBox() {
+  document.getElementById("box").style.backgroundColor = "green";
+}
+
+
+function fontText() {
+  document.getElementById("text2").style.fontSize = "30px";
+}
+
+
+function sizeImage() {
+  document.getElementById("img1").style.width = "300px";
+}
+
+function Border() {
+  document.getElementById("box2").classList.toggle("border");
+}
